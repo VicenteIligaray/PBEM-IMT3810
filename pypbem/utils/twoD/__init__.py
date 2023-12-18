23eca15329683db14aed739fca969818a19e416e
VicenteIligaray <vicente@DESKTOP-2RA8FV9.localdomain>
2023-12-17 21:09:09 -0300

    Added my project

---
+++ pypbem/utils/twoD/__init__.py
@@ -0,0 +1,5 @@
+from .circle_grid import get_circle_grid
+from .green_evaluation import green_eval, green_eval2
+from .BSLO import assemble_BSLO
+from .RHS import assemble_RHS
+from .potential import potential_eval
